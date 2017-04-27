2495e39219e579fd7fc0086747b238e2d80b49a3
Michael Chang <cbhl@google.com>
2017-04-27 16:29:12 -0700

    Attempt to add IR, but it doesn't work yet.

--- main.ino
+++ main.ino
@@ -1,3 +1,4 @@
+#include <SoftwareSerial.h>
 #include "FastLED.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_PCD8544.h"
@@ -13,8 +14,23 @@
 #define pin_lcd_di 11
 #define pin_lcd_clk 13
 #define pin_lcd_led 9 // not yet
+#define pin_ir_rx 10
+#define pin_ir_tx 12
+
+SoftwareSerial irSerial(pin_ir_rx, pin_ir_tx); // RX, TX
+const char _h[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+void lcd_print(Adafruit_PCD8544 lcd, char* s) {
+  for (unsigned int i=0; i<strlen(s); i++) {
+    lcd.write(s[i]);
+  }
+}
 
 void setup() {
+  irSerial.begin(57600);
+  Serial.begin(115200);
+  Serial.print(F("Hello\n"));
+
   Adafruit_PCD8544 lcd(pin_lcd_clk, pin_lcd_di, pin_lcd_dc, pin_lcd_ce, pin_lcd_rst);
 
   lcd.begin();
@@ -26,6 +42,7 @@ void setup() {
   CRGB leds[8];
   FastLED.addLeds<WS2812B, pin_rgb>(leds, sizeof(leds));
   byte t = 0;
+  unsigned char c = 255;
   while (1) {
     t++;
     for(byte i=0; i < 8; i++) {
@@ -45,6 +62,31 @@ void setup() {
     lcd.write('0' + (t / 100) % 10);
     lcd.write('0' + (t / 10) % 10);
     lcd.write('0' + (t / 1) % 10);
+
+    lcd.write('I');
+    if (irSerial.available()) {
+      lcd.write('R');
+      unsigned char r = irSerial.read();
+      Serial.println(r, HEX);
+      if (r == 0) {
+        lcd.write('0');
+      } else {
+        c = r;
+      }
+    }
+    lcd.write('\n');
+    lcd.write('I');
+    lcd.write('R');
+    lcd.write(':');
+    lcd.write(' ');
+    lcd.write('0');
+    lcd.write('x');
+    if (c >= 16) {
+      lcd.write(_h[c / 16]);
+    }else{
+      lcd.write('0');
+    }
+    lcd.write(_h[c % 16]);
 
   lcd.display();
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 #BOARD_TAG    = nano328
 BOARD_TAG    = nano
 BOARD_SUB = atmega328
-ARDUINO_LIBS = FastLED Adafruit-GFX-Library Adafruit-PCD8544-Nokia-5110-LCD-library SPI
+ARDUINO_LIBS = FastLED Adafruit-GFX-Library Adafruit-PCD8544-Nokia-5110-LCD-library SPI SoftwareSerial
 USER_LIB_PATH := $(realpath .)
 MONITOR_BAUDRATE = 115200
 
